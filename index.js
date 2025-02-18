a23b63f735a29a18dd95b366f54d9de7949986f2
Kayla Gentry <kaylagentry@Kaylas-MacBook-Pro.local>
2025-02-17 21:52:18 -0500

    part 3

--- index.js
+++ index.js
@@ -103,10 +103,65 @@ console.log(fionna.companion);
 console.log(fionna.companion.companion);
 
 
+//!========================== PART 3: ==================================
 
 
+class Adventurer extends Character{
+    constructor (name, role){
+        super(name);
+
+        //adventeres have specialized roles
+        this.role = role;
+
+        // every adventurer starts with a bedroll and 50 gold coins
+        this.inventory.push("bedroll", "100 gold coins", "strength booster", "spell book", "sword" );
+    }
+    // Adventurers have the ability to scout ahead of them
+    scout () {
+        console.log(`${this.name} is scouting ahead.. `);
+        super.roll();
+    }
+};
+
+class Companion extends Character {
+    constructor (name, role, type){
+        super(name);
+        this.type = type;
+        this.role = role;
+        this.loyalty = 100;
+        this.inventory.push("loyalty", "assist", "healer")
+    };
+    assist(){
+        console.log(`${this.name} is assisting...`);
+        super.roll();
+    }
+    heal(){
+        console.log(`${this.name} is healing...`);
+        this.health = Math.min(this.health + 20, 100) // healing properties add 20 points, but max health is 100.
+        
+    }
+}
+
+// create a new adventurer
+const fin = new Adventurer("Fin", "Warior");
+fin.roll();
+fin.scout();
+
+// createw a new companion
+const jake = new Companion("Jake", "Best Friend", "Dog");
+jake.roll();
+jake.assist();
+jake.heal();
+
+console.log(`Jake's health is: ${jake.health}`);
+console.log(jake.type);
 
 
+const ladyRainicorn = new Companion("Lady", "Ally", "unicorn");
+console.log(ladyRainicorn.type);
+
+//!========================== PART 4: ===============================
+console.log("===================================================");
 
 
 
